d01c9a94293bdf83edc2148adbdfb0d0a2b99f2f
Ekerot <daniel.ekerot@gmail.com>
2016-11-12 23:45:05 +0100

    scraper with working date fetcher and ajaxrecall fetcher

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 'use strict';
 
+let scraper = require('./lib/scraper');
+let test = require('./lib/checkFreeWeekendDay')
+
 /**
  * Created by ekerot on 2016-11-08.
  */
 
-console.log('Hello world!')
+let initialLinks = scraper.initialScrape('http://vhost3.lnu.se:20080/weekend');
+
+
