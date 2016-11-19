d7dcd934b2daefa827c4e26276a9cc4defc90381
vagrant <vagrant@precise32.(none)>
2016-11-19 13:04:02 +0000

    new version using pug

--- server.js
+++ server.js
@@ -3,18 +3,28 @@
  */
 
 let express = require('express');
+let startScraping = require('./lib/scraper');
 let app = express();
 
-app.use(express.static(__dirname));
+app.set('view engine', 'pug');
+app.use(express.static('public'));
 
-app.get('/', function(req, res){
-    res.sendfile(__dirname + '/index.html');
-});
-
-app.post('/', function (req, res) {
-    res.send('Got a POST request')
+app.get('/', function (req, res) {
+    res.render('index')
 })
+
+app.get('/process_get', function (req, res) {
+
+    response = {
+        url:req.query.url
+    };
+
+    if(response) {
+        console.log(startScraping(response));
+    }
+
+});
 
 app.listen(4000, function () {
     console.log('There is magic going on on port 4000!')
-})
+});
