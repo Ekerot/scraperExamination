009c38caac3cd599a3486d7e0fd2d2dcbbd76b21
Ekerot <daniel.ekerot@gmail.com>
2016-11-22 00:04:09 +0100

    dynamic movie titles

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
  * Created by ekerot on 2016-11-16.
  */
 
+/**
+ * Server listening to port 4000. Middlewear connecting to pug-files and the model.
+ */
+
 let express = require('express');
 let startScraping = require('./lib/scraper');
 let app = express();
@@ -11,9 +15,13 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
+//render html page
+
 app.get('/', function (req, res) {
     res.render('index')
 });
+
+//getting and sending response and result to the .pug-file.
 
 app.get('/process_get', function (req, res) {
 
@@ -26,6 +34,8 @@ app.get('/process_get', function (req, res) {
             res.render('index', {error});
         })
 });
+
+//listens to port 4000
 
 app.listen(4000, function () {
     console.log('There is magic going on on port 4000!')
